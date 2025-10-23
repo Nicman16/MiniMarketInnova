d982cce737d4ba3483113b21d0d6648076a0a87f
Nicman16 <nicolasdani3.ndrm@gmail.com>
2025-10-23 14:32:23 -0500

    correcion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './styles/App.css';
 import './styles/Global.css';
 import Login from './componentes/Login';
 import PuntoVenta from './componentes/PuntoVenta';
